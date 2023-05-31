709d0eaf7cf545ac0d5fda25b2fb3056a6819846
Kevin Ngo <kevoin.ngo@gmail.com>
2023-05-31 12:57:46 -0700

    finished categorical section

--- app.py
+++ app.py
@@ -1,18 +1,96 @@
 import streamlit as st
+import pandas as pd
+from matplotlib import pyplot as plt
+import io
 
-st.title('title of your application')
-st.markdown('for example here is a **bold text**')
+web_apps = st.sidebar.selectbox("Select Web Apps",
+                                ("Exploratory Data Analysis", "test"))
 
-st.sidebar.title('title of the sidebar')
 
-agree = st.checkbox('Click me')
+if web_apps == "Exploratory Data Analysis":
 
-if agree:
-    st.write('Great!')
-    st.markdown('This is *italic text*')
+  uploaded_file = st.sidebar.file_uploader("Choose a file")
 
-side_check = st.sidebar.checkbox('Click Checkbox')
-if side_check:
-    st.sidebar.write('Sidebar checkbox has been clicked')
+  if uploaded_file is not None:
+    # Can be used wherever a "file-like" object is accepted:
+    df = pd.read_csv(uploaded_file)
+    
+    st.write(f'Dataframe has shape {df.shape}')
 
-#python3 -m streamlit run app.py
+    show_dtypes = st.checkbox("Show data types of each column", key="disabled3", value = True)
+    if show_dtypes:
+      st.write(df.dtypes.rename("Data Types"))
+
+    show_df = st.checkbox("Show Data Frame", key="disabled")
+
+    if show_df:
+      st.write(df)
+
+    column_type = st.sidebar.selectbox('Select Data Type',
+                                       ("Numerical", "Categorical", "Bool", "Date"))
+
+    if column_type == "Numerical":
+      numerical_column = st.sidebar.selectbox(
+          'Select a Column', df.select_dtypes(include=['int64', 'float64']).columns)
+      # summary statistics
+      show_summary = st.checkbox("Show Summary Statistics", key = "disabled2", value = True)
+
+      if show_summary:
+        st.write(df[numerical_column].describe())
+      # histogram
+      choose_color = st.color_picker('Pick a Color', "#69b3a2")
+      choose_opacity = st.slider(
+          'Color Opacity', min_value=0.0, max_value=1.0, step=0.05, value = 1.0)
+
+      hist_bins = st.slider('Number of bins', min_value=5,
+                            max_value=150, value=30)
+      hist_title = st.text_input('Set Title', 'Histogram')
+      hist_xtitle = st.text_input('Set x-axis Title', numerical_column)
+
+      fig, ax = plt.subplots()
+      ax.hist(df[numerical_column], bins=hist_bins,
+              edgecolor="black", color=choose_color, alpha=choose_opacity)
+      ax.set_title(hist_title)
+      ax.set_xlabel(hist_xtitle)
+      ax.set_ylabel('Count')
+
+      st.pyplot(fig)
+      filename = "plot.png"
+      fig.savefig(filename,dpi = 300)
+
+      download_file_name = st.text_input('Set File Name For Download', 'plot')
+      # Display the download button
+      with open("plot.png", "rb") as file:
+        btn = st.download_button(
+            label="Download image",
+            data=file,
+            file_name=f"{download_file_name}.png",
+            mime="image/png"
+        )
+    # barplots (categorical)
+    elif column_type == "Categorical":
+        categorical_column = st.sidebar.selectbox('Select a Column', df.select_dtypes(include=['object']).columns)
+        choose_color = st.color_picker('Pick a Color', "#69b3a2")
+        choose_opacity = st.slider(
+          'Color Opacity', min_value=0.0, max_value=1.0, step=0.05, value = 1.0)
+        
+        fig, ax = plt.subplots()
+        ax.bar(x = df[categorical_column].value_counts().index, height = df[categorical_column].value_counts(), color=choose_color, alpha=choose_opacity, edgecolor = "black")
+        bar_title = st.text_input('Set Title', 'Barplot')
+        bar_xtitle = st.text_input('Set x-axis Title', categorical_column)
+        ax.set_title(bar_title)
+        ax.set_xlabel(bar_xtitle)
+        ax.set_ylabel('Count')
+        st.pyplot(fig)
+        filename = "plot.png"
+        fig.savefig(filename,dpi = 300)
+
+        download_file_name = st.text_input('Set File Name For Download', 'plot')
+        #display download button
+        with open("plot.png", "rb") as file:
+          btn = st.download_button(
+            label="Download image",
+            data=file,
+            file_name=f"{download_file_name}.png",
+            mime="image/png"
+        )
--- data_basics.py
+++ data_basics.py
@@ -1,2 +1,7 @@
 import numpy as np
+import pandas as pd
 
+rng = np.random.default_rng(123)
+d = rng.random((3,3))
+
+print(pd.Dataframe(data=d))
